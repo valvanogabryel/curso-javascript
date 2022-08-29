117c7b5f6023ce28cdc7a5d9718e60fe5c663c96
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-08-29 11:55:44 -0300

    jk

---
+++ aula09/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 70) {
+    console.log(`Você ultrapassou a velociade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança.`)
---
+++ aula09/ex009.js
@@ -0,0 +1,12 @@
+var país = 'Brasil'
+if (país == 'Brasil') {
+    console.log('Brasileiro!')
+} else {
+    console.log(`Estrangeiro! Vivendo em ${país}`)
+}
+
+if (país != 'Brasil') {
+    console.log(`Você é Estrangeiro!`)
+} else {
+    console.log(`Você é Brasileiro!`)
+}
