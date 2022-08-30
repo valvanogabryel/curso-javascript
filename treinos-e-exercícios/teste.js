707f7c6bf6dab0e492fb91065ce7d7f1b3c53d13
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-08-30 11:22:42 -0300

    k

---
+++ treinos-e-exercícios/teste.js
@@ -0,0 +1,10 @@
+var resposta = 'Talvez'
+
+console.log(`${resposta}`)
+if (resposta == 'Sim') {
+    console.log('Então tá show!')
+} else if (resposta == 'Talvez') {
+    console.log('Qual foi? Pensa melhor.')
+} else {
+    console.log('Então vai tomar no CÚ!')
+}
