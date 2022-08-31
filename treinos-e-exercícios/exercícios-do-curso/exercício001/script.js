93ee2cf7a00b114ce722812b4e7218f62e91a928
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-08-31 12:46:09 -0300

    h

--- treinos-e-exercícios/exercícios-do-curso/exercício001/script.js
+++ treinos-e-exercícios/exercícios-do-curso/exercício001/script.js
@@ -3,12 +3,15 @@ function carregar() {
     var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas!`
+    msg.innerHTML = `<p>Agora são ${hora} horas!</p>`
     if (hora >= 0 && hora < 12) {
         img.src = 'img/foto-manha.png'
+        document.body.style.background = '#e1b28f'
     } else if (hora >= 12 && hora < 18) {
         img.src = 'img/foto-tarde.png'
+        document.body.style.background = '#9B701B'
     } else {
         img.src = 'img/foto-noite.png'
+        document.body.style.background = '#362D4C'
     }
 }
