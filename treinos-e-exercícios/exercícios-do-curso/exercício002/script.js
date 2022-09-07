3bc34bc97e820b503eb50e8ebe6ffcf9465b3aec
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-09-07 15:57:22 -0300

    m

---
+++ treinos-e-exercícios/exercícios-do-curso/exercício002/script.js
@@ -0,0 +1,60 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.length == 0 || Number(fano.value) > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente')
+    } else if (Number(fano.value) < 1922) {
+        alert('[ERRO] Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 14) {
+                img.setAttribute('src', 'img/foto-bebe-m.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'img/foto-jovem-m.png')
+            } else if (idade < 60) {
+                img.setAttribute('src', 'img/foto-adulto-m.png')
+            } else {
+                img.setAttribute('src', 'img/foto-idoso-m.png')
+            }
+        } else {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 14) {
+                img.setAttribute('src', 'img/foto-bebe-f.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'img/foto-jovem-f.png')
+            } else if (idade < 60) {
+                img.setAttribute('src', 'img/foto-adulto-f.png')
+            } else {
+                img.setAttribute('src', 'img/foto-idoso-f.png')
+            }
+        }
+
+
+
+
+
+
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
