7ff1a740ed9e219758251db36aed7b76b7c6481e
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-09-07 16:39:24 -0300

    Update ambiente.js

--- aula11/ambiente.js
+++ aula11/ambiente.js
@@ -1,31 +1,19 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var c = 1 //contador
+while (c < 5) {
+    console.log(`Passo ${c}`)           //RECOMENDADO
+    c++ // c = c + 1
+}
+// OU
+do {
+    console.log(`Passo ${c}`)
+    c++                                 //USÁVEL
+} while (c < 5)
 
 
 /*console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
-console.log('Tudo bem?')
+console.log('Tudo bem?')                //PÉSSIMO
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 */
