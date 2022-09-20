43d5190cb08a90bbabf1dff9952212068b8e9e46
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-09-20 11:14:53 -0300

    f

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -22,3 +22,9 @@ console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 
+
+
+for (let pos = 0; pos < num.length; pos++) {
+    console.log(num[pos])
+}
+
---
+++ aula13/ambiente2.js
@@ -0,0 +1,25 @@
+let value = [10, 8, 1, 5, 4, 6]
+
+//console.log(valores)              Forma não formatada
+
+/*
+console.log(valores[0])
+console.log(valores[1]) 
+console.log(valores[2])             Forma burra
+console.log(valores[3])
+console.log(valores[4])
+...
+*/
+
+for (let pos = 0; pos < value.length; pos++) {
+    console.log(`A posição (índice) ${pos} tem o valor ${value[pos]}`)
+    //                                   Forma prática (1)
+}
+
+console.log('----------------------------------')
+
+
+for (let pos in value) {
+    console.log(`A posição (índice) ${pos} tem o valor ${value[pos]}`)
+    //                                  Forma prática (2)... Mais indicada, mas não primordial
+}
