fe9c657417a0a66f007571feb0ac84f5ed80c634
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-09-20 11:01:35 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+//Array/Vetor//
+
+
+let num = [5, 10, 20, 3, 9]
+// índice: 0  1   2   3  4
+
+
+
+console.log(num[4])
+
+num.push(88) // com o "push" o índice [5] foi adicionado
+
+console.log(num[5])
+
+console.log(num.length) //Número de elementos dentro do Array
+
+num.sort() // Sorteia a posição dos números
+
+console.log(num)
+
+
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
