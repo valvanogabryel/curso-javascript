5323b3515c9a8930500a8b2c2a1b98fb805fda9d
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-09-21 10:26:28 -0300

    Update script.js

---
+++ treinos-e-exercícios/exercícios-do-curso/exercício005/script.js
@@ -0,0 +1,32 @@
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
+
+function isNum(n) {
+    if (Number(n) >= 1 || Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function isLista(n, l) {
+    if (
+
+
+
+        function adicionar() {
+            if (isNum(num.value) && !isLista(num.value)) {
+                alert('Tudo Ok!')
+            } else {
+                alert('Valor inválido ou já encontrado na lista.')
+            }
+
+
+
+        }
+
+function finalizar() {
+
+        }
